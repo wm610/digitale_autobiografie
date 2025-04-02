5762f9dc8268a9e55b912a1f58f62897e3588084
Benno Nederkorn <benno.nederkorn@osnanet.de>
2025-04-02 10:28:42 +0200

    changed test for Raspberry

--- script/raspberry_test.py
+++ script/raspberry_test.py
@@ -1,24 +1,15 @@
-import RPi.GPIO as GPIO
-import time
+from gpiozero import Button
+from signal import pause
 
-# Set GPIO mode to BCM (use GPIO numbers, not pin numbers)
-GPIO.setmode(GPIO.BCM)
+# Define button connected to GPIO 17
+button = Button(17)
 
-# Define button GPIO pin
-BUTTON_PIN = 17  # Change this to your actual GPIO pin
+# Function to run when button is pressed
+def button_pressed():
+    print("Button Pressed!")
 
-# Set up the button as an input with a pull-up resistor
-GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+# Attach event listener
+button.when_pressed = button_pressed
 
-try:
-    print("Press the button...")
-    while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.LOW:  # Button is pressed
-            print("Button Pressed!")
-            time.sleep(0.2)  # Debounce delay
-
-except KeyboardInterrupt:
-    print("Exiting...")
-
-finally:
-    GPIO.cleanup()  # Reset GPIO settings on exit
+print("Waiting for button press...")
+pause()  # Keeps the program running
