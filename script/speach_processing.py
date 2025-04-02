b1d9d1aa47664f403414edab42bc89969c965fcb
Martin Waxenberger <martin.waxenberger.610@gmail.com>
2025-04-03 00:54:23 +0200

    bugfix typecheck file generation

--- script/speach_processing.py
+++ script/speach_processing.py
@@ -12,13 +12,14 @@ class SpeachProcessing:
         self.current_directory = Path.cwd()
         self.recordings_path = self.current_directory / "recordings"
         self.textfiles_path = self.current_directory / "textfiles"
-    
+        self.samplerate = 48000 #Sample rate in Hz, 44100 on Windows
+ 
     def create_wav_file(self, audio_data, current_question_index, current_category: str) -> Path:
         """
         take in audio data and return its path after saving
         """
         audio_data_np = np.concatenate(audio_data, axis=0)
-        filename = f"{self.recordings_path}/{current_question_index}_{current_category}.wav"
+        filename = self.recordings_path / f"{current_question_index}_{current_category}.wav"
         wav.write(filename, self.samplerate, audio_data_np)
         return filename
 
@@ -27,14 +28,14 @@ class SpeachProcessing:
         take in audio data path, create transcription and return textfile path 
         """
         self.textfiles_path.mkdir(exist_ok=True)
-        textfilename = f"{self.textfiles_path}/{current_question_index}_{current_category}.txt"
+        textfilename = self.textfiles_path / {current_question_index}_{current_category}.txt"
         segments = self.model.transcribe(filepath_wav, beam_size=5, vad_filter=True)
         with open(textfilename, 'w', encoding='utf-8') as file:
             for segment in segments:
                 file.write(f"{segment.text}\n")
         return textfilename
     
-    def audio_diagnostic_info():
+    def audio_diagnostic_info(self):
         """Print diagnostic information about the audio input device."""
         default_device = sd.default.device[0]  # Index of the default input device
         device_info = sd.query_devices(default_device)  # Get device details
