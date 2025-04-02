2eadc2e3e7c995e1e6bc5b970dbcb1c2a9044ad5
Martin Waxenberger <martin.waxenberger.610@gmail.com>
2025-04-02 14:59:03 +0200

    added answer collecting to setup.py

--- script/setup.py
+++ script/setup.py
@@ -1,4 +1,5 @@
 import logging
+from pathlib import Path
 
 def get_logger():
     # Configure the logger
@@ -11,3 +12,14 @@ def get_logger():
         ]
     )
     return logging.getLogger("BasicLogger")
+
+def collect_answers_into_single_string(answers_txt : list[Path]):
+    """
+    collects all the existing answers and strings them into a single string
+
+    answers_txt: List of Paths to answer files.
+    """
+    answers_content = [answer.read_text(encoding="utf-8") for answer in answers_txt]
+    all_answers = " ".join(answers_content)
+
+    return all_answers
